0ad3c2de2ad2acb2247b226449e4d323f91802df
yairtdas <yairsmitharaujo@gmail.com>
2026-06-20 10:43:29 -0500

    Actualizar siteUrl al dominio de produccion

--- src/lib/data.ts
+++ src/lib/data.ts
@@ -39,6 +39,6 @@ export const personalInfo = {
   email: 'yair.araujo@tecsup.edu.pe',
   github: 'https://github.com/yag-smith',
   linkedin: 'https://www.linkedin.com/in/yair-smith-araujo-gabriel-281228326/',
-  siteUrl: 'https://tuportafolio.com',
+  siteUrl: 'https://mini-spa-react-5yjw.vercel.app',
   avatar: 'https://plus.unsplash.com/premium_photo-1689568126014-06fea9d5d341?q=80&w=1740&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
 };
